4e9a729828e454e342fb80462d03c60ab469ad14
Madeline LeClair <madelineleclair@gmail.com>
2017-05-29 20:27:09 -0400

    Add logic to controller for pricing

--- app/controllers/tickle/pricings_controller.rb
+++ app/controllers/tickle/pricings_controller.rb
@@ -3,7 +3,18 @@ require_dependency "tickle/application_controller"
 module Tickle
   class PricingsController < ApplicationController
     def create
-    @price = Pricing.calculate_price(pricing_params)
+      params = pricing_params
+
+      @errors = []
+      @errors << age_errors(params["age"])
+      @errors << gender_errors(params["gender"])
+
+      if @errors.empty?
+        @price = calculate_price(params)
+        render :create
+      else
+        redirect_to pricings_url
+      end
     end
 
     def index
@@ -13,6 +24,53 @@ module Tickle
     private
     def pricing_params
       params.require(:pricings).permit(:gender, :age, conditions: [])
+    end
+
+    def calculate_price(params)
+      @price = 100
+      @price += age_cost(params["age"].to_i)
+      @price = @price * conditions(params["conditions"]) + @price
+      @price -= gender_deduction(params["gender"])
+    end
+
+    def age_cost(age)
+      (age - 18) / 5 * 20
+    end
+
+    def conditions(conditions)
+      return 0 if conditions.nil?
+      total = 0
+      conditions.each do |condition|
+        case condition
+        when "Allergies"
+          total += 0.01
+        when "Sleep Apnea"
+          total += 0.06
+        when "Heart Disease"
+          total += 0.17
+        else
+          total += 0
+        end
+      end
+      total
+    end
+
+    def gender_deduction(gender)
+      gender == "Female" ? 12 : 0
+    end
+
+    def age_errors(age)
+      if age.nil?
+        @errors << "Age cannot be blank."
+      elsif age.to_i < 18
+        @errors << "You can only get a quote if you are 18 or over"
+      end
+    end
+
+    def gender_errors(gender)
+      if params["gender"].nil?
+        @errors << "Gender can't be blank"
+      end
     end
   end
 end
--- app/models/tickle/pricing.rb
+++ app/models/tickle/pricing.rb
@@ -3,32 +3,10 @@ module Tickle
     def  self.calculate_price(params)
       debugger
       total = 100
-      total += age_cost(age)
-      total = total * conditions(conditions) + total
-      total -= gender_deduction(gender)
+      total += self.age_cost(params["age"])
+      total = total * conditions(params["conditions"]) + total
+      total -= gender_deduction(params["gender"])
     end
 
-    def age_cost(age)
-      (age - 18) / 5 * 20
-    end
-
-    def conditions(conditions)
-      total = 0
-      conditions.each do |condition|
-        case condition
-        when "Allergies"
-          total += 0.01
-        when "Sleep Apnea"
-          total += 0.06
-        when "Heart Disease"
-          total += 0.17
-        end
-      end
-      total
-    end
-
-    def gender_deduction(gender)
-      gender == "Female" ? 12 : 0
-    end
   end
 end
