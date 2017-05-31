d37c246cf626b406e7bb0cecd37ee7bfcc38b8d7
Madeline LeClair <madelineleclair@gmail.com>
2017-05-31 14:42:27 -0400

    Add flash errors to specs

--- spec/controllers/tickle/pricings_controller_spec.rb
+++ spec/controllers/tickle/pricings_controller_spec.rb
@@ -11,31 +11,38 @@ module Tickle
       end
     end
     describe "POST #create" do
-      context "if age and gender are provided by user" do
-        it "renders the pricings create" do
-          post :create, params: { pricings: { age: 18, gender: "Female" } }
-          expect(response).to render_template(:create)
+      context "with valid params" do
+        context "if age and gender are provided by user" do
+          it "renders the pricings create" do
+            post :create, params: { pricings: { age: 18, gender: "Female" } }
+            expect(response).to render_template(:create)
+          end
         end
       end
 
-      context "if age and gender are not given by user" do
-        it "redirect to pricings index" do
-          post :create, params: { pricings: { age: "" } }
-          expect(response).to redirect_to(pricings_url)
+      context "with invalid params" do
+        context "if age and gender are not given by user" do
+          it "redirect to pricings index" do
+            post :create, params: { pricings: { age: "" } }
+            expect(response).to redirect_to(pricings_url)
+            expect(flash[:errors]).to be_present
+          end
         end
-      end
 
-      context "if age is not given by user" do
-        it "redirect to pricings index" do
-          post :create, params: { pricings: { age: "", gender: "Male" } }
-          expect(response).to redirect_to(pricings_url)
+        context "if age is not given by user" do
+          it "redirect to pricings index" do
+            post :create, params: { pricings: { age: "", gender: "Male" } }
+            expect(response).to redirect_to(pricings_url)
+            expect(flash[:errors]).to be_present
+          end
         end
-      end
 
-      context "if gender is not given by user" do
-        it "redirect to pricings index" do
-          post :create, params: { pricings: { age: 35 } }
-          expect(response).to redirect_to(pricings_url)
+        context "if gender is not given by user" do
+          it "redirect to pricings index" do
+            post :create, params: { pricings: { age: 35 } }
+            expect(response).to redirect_to(pricings_url)
+            expect(flash[:errors]).to be_present
+          end
         end
       end
     end
