9dfaa1f34938d26d250a551ebddf732723a992aa
Madeline LeClair <madelineleclair@gmail.com>
2017-05-31 14:35:33 -0400

    Create initial specs for index and create

--- spec/controllers/tickle/pricings_controller_spec.rb
+++ spec/controllers/tickle/pricings_controller_spec.rb
@@ -13,21 +13,30 @@ module Tickle
     describe "POST #create" do
       context "if age and gender are provided by user" do
         it "renders the pricings create" do
-          post :create
-          expect(response).to be_success
+          post :create, params: { pricings: { age: 18, gender: "Female" } }
+          expect(response).to render_template(:create)
         end
       end
 
       context "if age and gender are not given by user" do
-        it "redirect to pricings index"
+        it "redirect to pricings index" do
+          post :create, params: { pricings: { age: "" } }
+          expect(response).to redirect_to(pricings_url)
+        end
       end
 
       context "if age is not given by user" do
-        it "redirect to pricings index"
+        it "redirect to pricings index" do
+          post :create, params: { pricings: { age: "", gender: "Male" } }
+          expect(response).to redirect_to(pricings_url)
+        end
       end
 
       context "if gender is not given by user" do
-        it "redirect to pricings index"
+        it "redirect to pricings index" do
+          post :create, params: { pricings: { age: 35 } }
+          expect(response).to redirect_to(pricings_url)
+        end
       end
     end
   end
